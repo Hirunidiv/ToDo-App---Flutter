9eda190d3c554435afec9e2d4a66e587d4d55090
Hirunidiv <hirunidivyanjalee365@gmail.com>
2025-02-16 10:14:24 +0530

    build appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,33 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:todo_app_flutter/constants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}): super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(), // Calling the method
+      body: Container(
+        child: Text('This is home screen.'),
+      ),
+    );
+  }
+
+  // Method that returns the AppBar
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           Icon(
             Icons.menu,
             color: tdBlack,
-            size: 30,),
+            size: 30,
+          ),
           Container(
             height: 40,
             width: 40,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: Image.asset('assets/images/profileImg.jpg'),
-              ),
+            ),
           ),
-        ],),
-      ),
-      body: Container(
-        child: Text('This is home screen.'),
+        ],
       ),
     );
   }
