df3036e5ad430624d06d2e27fc7d7713c24fdd17
Hirunidiv <hirunidivyanjalee365@gmail.com>
2025-02-16 14:20:56 +0530

    create search bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,9 +10,41 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(), // Calling the method
       body: Container(
-        child: Text('This is home screen.'),
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        child: Column(
+          children: [
+           searchBox(),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget searchBox(){
+    return  Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: tdBlack,
+                    size: 20,
+                    ),
+                    prefixIconConstraints: BoxConstraints(
+                      maxHeight: 20, 
+                      minWidth: 25
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Search',
+                      hintStyle: TextStyle(color: tdGrey)
+                ),
+              ),
+            );
   }
 
   // Method that returns the AppBar
